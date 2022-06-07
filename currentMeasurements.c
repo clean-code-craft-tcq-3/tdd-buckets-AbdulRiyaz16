29b8970bb193eeca5ffd0f26fcef8adb049d5249
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-06-08 02:06:05 +0530

    Update currentMeasurements.c

--- currentMeasurements.c
+++ currentMeasurements.c
@@ -5,9 +5,10 @@
 
 int *sortArray(int *inputArray, int size)
 {
+    int temp = 0;    
     for (int i = 0; i < size; i++)
     {     
-        for (int j = i+1; j < length; j++)
+        for (int j = i+1; j < size; j++)
         {     
            if(inputArray[i] > inputArray[j])
            {    
