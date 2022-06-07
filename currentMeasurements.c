9a0929058a96750ac0bf2d7be1c1f37921b0451e
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-06-08 02:00:54 +0530

    Update currentMeasurements.c

--- currentMeasurements.c
+++ currentMeasurements.c
@@ -3,8 +3,26 @@
 #include <stdlib.h>
 #include "currentMeasurements.h"
 
+int *sortArray(int *inputArray, int size)
+{
+    for (int i = 0; i < size; i++)
+    {     
+        for (int j = i+1; j < length; j++)
+        {     
+           if(inputArray[i] > inputArray[j])
+           {    
+               temp = inputArray[i];    
+               inputArray[i] = inputArray[j];    
+               inputArray[j] = temp;    
+           }     
+        }     
+    } 
+    return inputArray;
+}
+
 int getRange(int *currentSamples)
 {
   int length =(int)ARRAYLENGTH(currentSamples);
+  currentSamples=sortArray(currentSamples,length);
   return 0;
 }
